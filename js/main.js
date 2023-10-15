76c11018157e361302767d7de2352dadabbbcc0b
David <eldarmanzefirka@gmail.com>
2023-10-15 11:05:56 +0300

    create file directories

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const row = 6
+const tab = 5
+function documentsReady(){
+
+}
+
+function createCell(){
+    for(let i = 0; i < row; i++){
+        for(let j = 0; i < tab; j++){
+            
+        }
+    }
+}
