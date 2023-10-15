f6af01ca004eb3191e033a3aa10750b47df85791
David <eldarmanzefirka@gmail.com>
2023-10-15 14:03:02 +0300

    (added) add game functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,114 @@
-const row = 6
-const tab = 5
+let instructionRead = false;
+let gameOver = false;
 function documentsReady(){
 
 }
+function ShowInstruction(){
+    alert("Правила гри, ви маєте таблицю 5х5, вам потрібно обрати числа в послідовності від 1 до 10, якщо ви оберете не правильне число то гра почнеться с початку, якщо не встигнете по часу то також гра почнеться спочатку, Гарної гри!!!")
+    instructionRead = true; 
+    game(); 
+}
 
-function createCell(){
-    for(let i = 0; i < row; i++){
-        for(let j = 0; i < tab; j++){
-            
+function game() {
+    const table = $("#game-table");
+    const usedNumbers = new Set();
+    let targetNumber = 1; 
+    let gameTimer = 60; 
+    const timerElement = $("#timer");
+    
+    function getRandomUniqueNumber() {
+        let randomNumber;
+        do {
+            randomNumber = Math.floor(Math.random() * 25) + 1;
+        } while (usedNumbers.has(randomNumber));
+        usedNumbers.add(randomNumber);
+        return randomNumber;
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const row = $("<tr></tr>");
+        for (let j = 0; j < 5; j++) {
+            const cell = $("<td></td>").text(getRandomUniqueNumber());
+            cell.css({color: "blue"});
+            row.append(cell);
+        }
+        table.append(row);
+    }
+
+    function updateTimer() {
+        timerElement.text(`Залишилось: ${gameTimer} секунд`);
+        gameTimer--;
+
+        if (gameTimer < 0) {
+            alert("Час вичерпано! Гра закінчена");
+            clearInterval(timerInterval);
         }
     }
+
+    const timerInterval = setInterval(updateTimer, 1000);
+
+    table.on("click", "td", function() {
+        if (gameOver) {
+            return;
+        }
+    
+        const cell = $(this);
+        const cellNumber = parseInt(cell.text());
+    
+        if (cellNumber === targetNumber) {
+            cell.css({ backgroundColor: "green", fontSize: "20px" }); 
+            targetNumber++;
+    
+            if (targetNumber === 11) {
+                alert("Знайдені усі числа! Ви перемогли");
+                clearInterval(timerInterval);
+                startNewGame();
+            }
+        } else {
+            $("#error-dialog").dialog("open");
+            startNewGame();
+        }
+    });
+    function startNewGame() {
+        usedNumbers.clear();
+        targetNumber = 1;
+        gameTimer = 60;
+        updateTimer();
+    
+        table.empty(); 
+    
+        for (let i = 0; i < 5; i++) {
+            const row = $("<tr></tr>");
+            for (let j = 0; j < 5; j++) {
+                const cell = $("<td></td>").text(getRandomUniqueNumber());
+                row.append(cell);
+            }
+            table.append(row);
+        }
+        
+        gameOver = false;
+    };
+    $("#end_game").on("click", function() {
+        startNewGame();
+    });
+       
 }
+
+
+
+
+$("#error-dialog").dialog({
+    autoOpen: false,
+    modal: true,
+    buttons: {
+        "OK": function() {
+            $(this).dialog("close");
+            resetGame();
+        }
+    }
+});
+$("#instruction").on("click", function() {
+    startGameAfterInstruction(); 
+});
+
+
